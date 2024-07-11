d3244efd201d0cfd18c5d1b9ee78a4e402484158
Eldar Faaiz <iniakunbisnisku@gmail.com>
2024-07-12 04:06:35 +0700

    update

--- packages/hardhat/hardhat.config.ts
+++ packages/hardhat/hardhat.config.ts
@@ -12,11 +12,12 @@ import "hardhat-deploy-ethers";
 
 // If not set, it uses ours Alchemy's default API key.
 // You can get your own at https://dashboard.alchemyapi.io
-const providerApiKey = "process.env.ALCHEMY_API_KEY";
+const providerApiKey = process.env.ALCHEMY_API_KEY || "oKxs-03sij-U_N0iOlrSsZFr29-IqbuF";
 // If not set, it uses the hardhat account 0 private key.
-const deployerPrivateKey = "process.env.DEPLOYER_PRIVATE_KEY";
+const deployerPrivateKey =
+  process.env.DEPLOYER_PRIVATE_KEY ?? "0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80";
 // If not set, it uses ours Etherscan default API key.
-const etherscanApiKey = "process.env.ETHERSCAN_API_KEY";
+const etherscanApiKey = process.env.ETHERSCAN_API_KEY || "DNXJA8RX2Q3VZ4URQIWP7Z68CJXQZSC6AW";
 
 const config: HardhatUserConfig = {
   solidity: {
@@ -41,12 +42,12 @@ const config: HardhatUserConfig = {
     // If the network you are looking for is not here you can add new network settings
     hardhat: {
       forking: {
-        url: `https://base-mainnet.alchemyapi.io/v2/${providerApiKey}`,
+        url: `https://mainnet.base.org`,
         enabled: process.env.MAINNET_FORKING_ENABLED === "true",
       },
     },
     base: {
-      url: `https://base-mainnet.alchemyapi.io/v2/${providerApiKey}`,
+      url: "https://mainnet.base.org",
       accounts: [deployerPrivateKey],
     },
     baseSepolia: {
