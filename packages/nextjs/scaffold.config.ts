7c8e6e97a67d3ef93994f26d79d71cd5aa94ee64
Eldar Faaiz <iniakunbisnisku@gmail.com>
2024-07-12 12:11:30 +0700

    update

--- packages/nextjs/scaffold.config.ts
+++ packages/nextjs/scaffold.config.ts
@@ -20,13 +20,13 @@ const scaffoldConfig = {
   // You can get your own at https://dashboard.alchemyapi.io
   // It's recommended to store it in an env variable:
   // .env.local for local testing, and in the Vercel/system env config for live apps.
-  alchemyApiKey: "process.env.NEXT_PUBLIC_ALCHEMY_API_KEY",
+  alchemyApiKey: "process.env.ALCHEMY_API_KEY",
 
   // This is ours WalletConnect's default project ID.
   // You can get your own at https://cloud.walletconnect.com
   // It's recommended to store it in an env variable:
   // .env.local for local testing, and in the Vercel/system env config for live apps.
-  walletConnectProjectId: "process.env.NEXT_PUBLIC_WALLET_CONNECT_PROJECT_ID",
+  walletConnectProjectId: "process.env.WALLET_CONNECT_PROJECT_ID",
 
   // Only show the Burner Wallet when running on hardhat network
   onlyLocalBurnerWallet: false,
