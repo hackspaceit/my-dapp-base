4cb3b2b58bc9bca4b6cca2cc2028fea8f482f15a
Eldar Faaiz <iniakunbisnisku@gmail.com>
2024-07-12 14:35:56 +0700

    update

--- packages/hardhat/hardhat.config.ts
+++ packages/hardhat/hardhat.config.ts
@@ -12,11 +12,11 @@ import "hardhat-deploy-ethers";
 
 // If not set, it uses ours Alchemy's default API key.
 // You can get your own at https://dashboard.alchemyapi.io
-const providerApiKey = "process.env.ALCHEMY_API_KEY";
+const providerApiKey = process.env.ALCHEMY_API_KEY || "";
 // If not set, it uses the hardhat account 0 private key.
-const deployerPrivateKey = "process.env.DEPLOYER_PRIVATE_KEY";
+const deployerPrivateKey = process.env.DEPLOYER_PRIVATE_KEY ?? "";
 // If not set, it uses ours Etherscan default API key.
-const etherscanApiKey = "process.env.ETHERSCAN_API_KEY";
+const etherscanApiKey = process.env.ETHERSCAN_API_KEY || "";
 
 const config: HardhatUserConfig = {
   solidity: {
