a85b3492a67ce2d6b751e98974e4c0781df90f2e
Eldar Faaiz <iniakunbisnisku@gmail.com>
2024-07-12 13:18:34 +0700

    update

--- packages/nextjs/components/Header.tsx
+++ packages/nextjs/components/Header.tsx
@@ -26,7 +26,7 @@ export const menuLinks: HeaderMenuLink[] = [
     href: "/",
   },
   {
-    label: "Debug Contracts",
+    label: "Debug Your Contracts",
     href: "/debug",
     icon: <BugAntIcon className="h-4 w-4" />,
   },
@@ -107,8 +107,8 @@ export const Header = () => {
             />
           </div>
           <div className="flex flex-col">
-            <span className="font-bold leading-tight">Scaffold-Base</span>
-            <span className="text-xs">Ethereum dev stack</span>
+            <span className="font-bold leading-tight">Optima</span>
+            <span className="text-xs">Decentralized Applications</span>
           </div>
         </Link>
         <ul className="hidden lg:flex lg:flex-nowrap menu menu-horizontal px-1 gap-2">
